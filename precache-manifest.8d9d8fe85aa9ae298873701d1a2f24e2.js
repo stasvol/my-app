987edf41b40294da3f3a0edc28ec434b21341a9b
stasvol <https://github.com/stasvol/my-test.git>
2021-05-22 00:16:24 +0300

    Updates

---
+++ precache-manifest.8d9d8fe85aa9ae298873701d1a2f24e2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cfd673b0a39e91885a1",
+    "url": "/my-app/static/css/main.5b622024.chunk.css"
+  },
+  {
+    "revision": "1cfd673b0a39e91885a1",
+    "url": "/my-app/static/js/main.8767f0ab.chunk.js"
+  },
+  {
+    "revision": "fa05bbed06c14711ddf9",
+    "url": "/my-app/static/js/runtime~main.755170b2.js"
+  },
+  {
+    "revision": "75b18ece11dc9ea444f7",
+    "url": "/my-app/static/js/2.cc92c3cf.chunk.js"
+  },
+  {
+    "revision": "4ade1e1da91e0bd65e18",
+    "url": "/my-app/static/css/3.20238418.chunk.css"
+  },
+  {
+    "revision": "4ade1e1da91e0bd65e18",
+    "url": "/my-app/static/js/3.12822ce0.chunk.js"
+  },
+  {
+    "revision": "72f333eda761f94c538f",
+    "url": "/my-app/static/css/4.d37511ba.chunk.css"
+  },
+  {
+    "revision": "72f333eda761f94c538f",
+    "url": "/my-app/static/js/4.eec572b2.chunk.js"
+  },
+  {
+    "revision": "035b86f39c90a75fb63a",
+    "url": "/my-app/static/css/5.d383d3b3.chunk.css"
+  },
+  {
+    "revision": "035b86f39c90a75fb63a",
+    "url": "/my-app/static/js/5.2ef43132.chunk.js"
+  },
+  {
+    "revision": "33a81bdcbe749f610823",
+    "url": "/my-app/static/js/6.779d3738.chunk.js"
+  },
+  {
+    "revision": "d6fd13bf6232273406fb2b65b3f2103f",
+    "url": "/my-app/static/media/kot.d6fd13bf.png"
+  },
+  {
+    "revision": "8e96d580a6c757525d39955c5e5a486a",
+    "url": "/my-app/static/media/user2.8e96d580.png"
+  },
+  {
+    "revision": "9ed071bbba3fb85ad35d3438947ad310",
+    "url": "/my-app/static/media/760.9ed071bb.svg"
+  },
+  {
+    "revision": "dc83c11b5d61c8742e15d6850696ff97",
+    "url": "/my-app/static/media/smail.dc83c11b.png"
+  },
+  {
+    "revision": "2ac8798e665b9d8cd721596d63cfdbd8",
+    "url": "/my-app/index.html"
+  }
+];
