56648926f2d41ba153db404e9de649336fa3a8cd
stas <stasv826gmail.com>
2022-03-26 03:41:21 +0200

    refagtoring

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React, {Component, Suspense} from 'react';
-import './App.css';
-import Header from './components/Header/Header'
-import {BrowserRouter, HashRouter, Switch, Route, Redirect} from "react-router-dom";
-import Music from './components/Music/Music';
-import News from './components/News/News';
-import Setting from './components/Settings/Setting';
-import NavContainer from "./components/Nav/NavContainer";
-// import { Switch } from "react-router"
-// import UserContainer from "./components/Users/UserContainer";
-// import DialogContainer from "./components/Dialogs/DialogContainer";
-// import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Login/login";
-import {withAuthRedirect} from "./Hoc/withAuthRedirect";
+import {BrowserRouter, withRouter, Switch, Route, Redirect} from "react-router-dom";
 import {connect, Provider} from "react-redux";
-import {authThunkCreator, loginOut, setAuthUserData} from "./redux/auth_reducer";
-import {withRouter} from 'react-router-dom';
 import {compose} from "redux";
-import {initializeApp} from "./redux/app_reducer";
-import Preloader from "./components/common/preloader/preloader";
+
 import store from "./redux/reduxStore";
 import {withLazySuspense} from "./Hoc/withLazySuspense";
-import Error from "./Error/error";
-// import state from './components/Settings/State'
-// import {addNewMessage,addNewText,updateNewText} from "./components/Settings/State"
-import Store from './components/Settings/State'
+import {initializeApp} from "./redux/app_reducer";
+import Music from './components/Music/Music';
+import NavContainer from "./components/Nav/NavContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/login";
 import SettingContainer from "./components/Settings/SettingContainer";
 import NewsContainer from "./components/News/NewsContainer";
+import Preloader from "./components/common/preloader/preloader";
+import Error from "./Error/error";
+
+import './App.css';
 
 const DialogContainer = React.lazy(() => import("./components/Dialogs/DialogContainer"));
 const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
@@ -36,18 +25,8 @@ const UserContainer = React.lazy(() => import("./components/Users/UserContainer"
 
 class App extends Component {
 
-
     componentDidMount(props) {
         this.props.initializeApp()
-        // this.props.authThunkCreator (this.props.id, this.props.email, this.props.login,this.props.isAuth)
-        // userApi.loginUser().then(data => {
-        //
-        //           if (data.resultCode === 0){
-        //               let {id, email, login} = data.data
-        //            this.props.setAuthUserData(id, email, login);
-        //        }
-        // });
-
     }
 
     render() {
@@ -58,11 +37,9 @@ class App extends Component {
         }
         return (
 
-            // <BrowserRouter>
             <div className="app-wrapper">
                 <HeaderContainer/>
                 <NavContainer/>
-                {/*<Navbar state={props.state.siteBar} />*/}
                 <div className={'app-pages'}>
 
                     <Suspense fallback={<div>Loading...</div>}>
@@ -70,45 +47,18 @@ class App extends Component {
                             <Route exact path={'/'} render={() => <Redirect to={'/Profile'} />}/>
                             <Route path={'/Dialogs'} render={() => <DialogContainer />}/>
                             <Route path={'/Profile/:userId?'} render={() => <ProfileContainer/>}/>
-
-
-
-                            {/*// <Dialogs  data={props.state.dialogPage}*/}
-                            {/*//                                               dispatch={props.dispatch} store={props.store}/>}/>*/}
-                            {/*// addMessage={props.addMessage}*/}
-                            {/*// addChangeNewMessage={props.addChangeNewMessage}/>}/>*/}
-                            {/*dispatch={props.dispatch}/>}/>*/}
-                            {/*// addPost={props.addPost}*/}
-                            {/*// addChangeText={props.addChangeTe} />}/>*/}
-
                             <Route path={'/User'} render={withLazySuspense(() => <UserContainer/>)}/>
-
-
                             <Route path={'/Music/vk'} render={() => <div>vk</div>}/>
                             <Route exact path={'/Music'} render={() => <Music/>}/>
-
                             <Route path={'/Login'} render={() => <Login/>}/>
-
                             <Route  path={'/News/:userId?'} render={() => <NewsContainer/>}/>
                             <Route path={'/Setting'} render={() => <SettingContainer />}/>
-                                                                             {/*newMessage={Store.getState().newMessage}*/}
-                                                                             {/*newPostMesText={Store.getState().newPostMesText}*/}
-                                 {/*addNewMessage={Store.addNewMessage.bind(Store)}*/}
-                                 {/*updateNewText={Store.updateNewText.bind(Store)}/>}/>*/}
-                                 {/*                                           dispatch={Store.dispatch.bind(Store)}/>}/>*/}
-                            {/*<Route path={'/Film'} render={ () =>  {return <div>FILM</div>}}/>*/}
-                            {/*<Dialogs />*/}
-                            {/*<Profile />*/}
-                            {/* <Music />*/}
-                            {/* <News />*/}
-                            {/* <Setting />*/}
+
                             <Route path={'*'} render={() => <Error/>}/>
                         </Switch>
                     </Suspense>
                 </div>
-
             </div>
-            // </BrowserRouter>
         );
     }
 }
@@ -124,22 +74,14 @@ let mapStateToProps = (state) => ({
 const AppContainer = compose(
     withRouter,
     connect(mapStateToProps, {initializeApp}))(App);
-// withRouter (connect ( mapStateToProps,{authThunkCreator}) (App));
-// export default App;
 
-let MyApp = (props) => {
-    return <React.StrictMode>
-        {/*<HashRouter>*/}
+let MyApp = () => (
+    <React.StrictMode>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
-                {/*<MyContext.Provider value={store}>*/}
                 <AppContainer state={store.getState()}/>
-                {/*addChangeText={store.addChangeText.bind(store)}*/}
-                {/*addMessage={store.addMessage.bind(store)} addChangeNewMessage={store.addChangeNewMessage.bind(store)}  */}
-                {/*</MyContext.Provider>*/}
             </Provider>
         </BrowserRouter>
-        {/*</HashRouter>*/}
     </React.StrictMode>
-}
+)
 export default MyApp
--- src/index.js
+++ src/index.js
@@ -1,48 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
-import store from './redux/reduxStore'
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
 import MyApp from "./App";
-import {subscribe} from "./components/Settings/State";
-import Store from './components/Settings/State'
-// import MyContext, {Provider} from "./MyContext";
 
+import './index.css';
 
-// const rerenderEntireTree = (state) => {
-//  const rerender =(Store)=> {
-ReactDOM.render(
-
-     <MyApp  />,
-
-    document.getElementById('root')
-)
-// }
-// rerender(Store);
-// Store.subscribe(rerender);
-// }
-// //
-// rerenderEntireTree(store.getState());
-// // // store.subscribe(rerenderEntireTree())
-// //
-// store.subscribe(() => {
-//     let state = store.getState();
-//     rerenderEntireTree(state);
-// });
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App state={state}  addPost={addPost}  addMessage={addMessage} />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-//
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//
+ReactDOM.render(<MyApp  />, document.getElementById('root'))
 reportWebVitals();
